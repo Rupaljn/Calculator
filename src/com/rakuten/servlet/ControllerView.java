9a6a1467833fb4b7b943a772cb4daa31f4074aa9
rupal <xyz@gmail.com>
2022-10-19 00:23:46 +0530

    First Version Calculator

---
+++ src/com/rakuten/servlet/ControllerView.java
@@ -0,0 +1,51 @@
+package com.rakuten.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ControllerView
+ */
+@WebServlet("/ControllerView")
+public class ControllerView extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ControllerView() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+String num1 = request.getParameter("num1");
+String num2 = request.getParameter("num2");
+try{
+	int n1 = Integer.parseInt(num1);
+	int n2 = Integer.parseInt(num2);
+	System.out.println(n1/n2);
+}
+catch(NumberFormatException n){
+	n.printStackTrace();
+	System.out.println("only enter numeric");
+}
+catch(ArithmeticException a){
+	a.printStackTrace();
+	System.out.println("please do not divied by zero");
+}
+
+	}
+}
